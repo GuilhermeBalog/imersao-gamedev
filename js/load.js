e27b8adf90fc98079373afb32546c530a099d3c5
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-06-29 12:31:21 -0300

    Corrige os caminhos de importação

--- js/load.js
+++ js/load.js
@@ -1,23 +1,23 @@
 function preload(){
-    imagemTelaInicial = loadImage('../images/assets/telaInicial.png')
-    fonteTelaInicial = loadFont('../images/assets/fonteTelaInicial.otf')
+    imagemTelaInicial = loadImage('images/assets/telaInicial.png')
+    fonteTelaInicial = loadFont('images/assets/fonteTelaInicial.otf')
 
-    imagemPersonagem = loadImage('../images/personagem/correndo.png')
+    imagemPersonagem = loadImage('images/personagem/correndo.png')
 
-    imagemInimigo = loadImage('../images/inimigos/gotinha.png')
-    imagemTroll = loadImage('../images/inimigos/troll.png')
-    imagemVoador = loadImage('../images/inimigos/gotinha-voadora.png')
+    imagemInimigo = loadImage('images/inimigos/gotinha.png')
+    imagemTroll = loadImage('images/inimigos/troll.png')
+    imagemVoador = loadImage('images/inimigos/gotinha-voadora.png')
 
-    imagemVida = loadImage('../images/assets/coracao.png')
+    imagemVida = loadImage('images/assets/coracao.png')
 
-    somDoJogo = loadSound('../sounds/trilha_jogo.mp3')
-    somDoPulo = loadSound('../sounds/somPulo.mp3')
+    somDoJogo = loadSound('sounds/trilha_jogo.mp3')
+    somDoPulo = loadSound('sounds/somPulo.mp3')
 
     for(let i = 0; i < 10; i++){
-        imagensParallax[i] = loadImage(`../images/cenario/paralax/${i + 1}.png`)
+        imagensParallax[i] = loadImage(`images/cenario/paralax/${i + 1}.png`)
     }
 
-    imagemGameOver = loadImage('../images/assets/game-over.png')
+    imagemGameOver = loadImage('images/assets/game-over.png')
 
-    fita = loadJSON('../fita/fita.json')
+    fita = loadJSON('fita/fita.json')
 }
